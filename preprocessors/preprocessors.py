51d8a25201511d247112d1e6e2ea2d7953089e25
Robert Hunt <Freely.Given.org@gmail.com>
2019-06-28 09:32:04 +1200

    Adjust index entries for OBS tQ and OBS tN

--- preprocessors/preprocessors.py
+++ preprocessors/preprocessors.py
@@ -290,7 +290,7 @@ class ObsNotesPreprocessor(Preprocessor):
                         file_contents = read_file(filepath)
                         file_basename = filename[:-3] # Remove the .md
                         markdown += f'\n# <a id="{story_number_string}-{file_basename}"/> {file_basename}\n\n' + file_contents
-                        toc_contents += f'  - title: "Segment {file_basename}"\n    link: {story_number_string}-{file_basename}\n\n'
+                        toc_contents += f'  - title: "{story_number_string}-{file_basename}"\n    link: {story_number_string}-{file_basename}\n\n'
                     else:
                         self.warnings.append(f"Unexpected '{filename}' file in 'content/{story_number_string}/'")
                 if markdown:
