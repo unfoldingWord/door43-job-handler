a72841a6148991dedb7e70aeaa0492f8cc1a513e
Robert Hunt <Freely.Given.org@gmail.com>
2019-08-20 18:43:51 +1200

    Improve logging for template download

--- door43_tools/project_deployer.py
+++ door43_tools/project_deployer.py
@@ -8,7 +8,7 @@ from shutil import copyfile
 from datetime import datetime, timedelta
 
 from rq_settings import prefix, debug_mode_flag
-from global_settings.global_settings import GlobalSettings
+from app_settings.app_settings import AppSettings
 from general_tools import file_utils
 from general_tools.file_utils import write_file, remove_tree
 from door43_tools.templaters import init_template
@@ -24,7 +24,7 @@ class ProjectDeployer:
     """
 
     def __init__(self, unzip_dir:str, temp_dir:str) -> None:
-        GlobalSettings.logger.debug(f"ProjectDeployer.__init__({unzip_dir}, {temp_dir})…")
+        AppSettings.logger.debug(f"ProjectDeployer.__init__({unzip_dir}, {temp_dir})…")
         self.unzip_dir = unzip_dir
         self.temp_dir = tempfile.mkdtemp(prefix='deployer_', dir=temp_dir)
 
@@ -45,7 +45,7 @@ class ProjectDeployer:
         :return bool:
         """
         start = time.time()
-        GlobalSettings.logger.debug(f"Deploying, build log: {json.dumps(build_log)[:256]} …")
+        AppSettings.logger.debug(f"Deploying, build log: {json.dumps(build_log)[:256]} …")
         assert 'multiple' not in build_log
         assert 'part' not in build_log
 
@@ -65,13 +65,13 @@ class ProjectDeployer:
         resource_type = build_log['resource_type']
         template_key = 'templates/project-page.html'
         template_file = os.path.join(template_dir, 'project-page.html')
-        GlobalSettings.logger.debug(f"Downloading {template_key} to {template_file} …")
-        GlobalSettings.door43_s3_handler().download_file(template_key, template_file)
+        AppSettings.logger.info(f"Downloading project page template from {AppSettings.door43_bucket_name} '{template_key}' to {template_file} …")
+        AppSettings.door43_s3_handler().download_file(template_key, template_file)
         source_dir, success = self.template_converted_files(build_log, output_dir, repo_name,
                                             resource_type, s3_commit_key, source_dir, start,
                                             template_file)
         if not success:
-            GlobalSettings.logger.critical("Templating failed -- returning False")
+            AppSettings.logger.critical("Templating failed -- returning False")
             return False
 
 
@@ -97,34 +97,34 @@ class ProjectDeployer:
         # Save master build_log.json
         build_log['ended_at'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
         file_utils.write_file(os.path.join(output_dir, 'build_log.json'), build_log)
-        GlobalSettings.logger.debug(f"Final build_log.json: {json.dumps(build_log)[:256]} …")
+        AppSettings.logger.debug(f"Final build_log.json: {json.dumps(build_log)[:256]} …")
 
         # Upload all files to the S3 door43.org bucket
-        GlobalSettings.logger.info(f"Uploading all files to the website bucket: {GlobalSettings.door43_bucket_name} …")
+        AppSettings.logger.info(f"Uploading all files to the website bucket: {AppSettings.door43_bucket_name} …")
         for root, _dirs, files in os.walk(output_dir):
             for filename in sorted(files):
                 filepath = os.path.join(root, filename)
                 if os.path.isdir(filepath):
                     continue
                 key = s3_commit_key + filepath.replace(output_dir, '').replace(os.path.sep, '/')
-                GlobalSettings.logger.debug(f"Uploading {filename} to {GlobalSettings.door43_bucket_name} bucket {key} …")
-                GlobalSettings.door43_s3_handler().upload_file(filepath, key, cache_time=0)
+                AppSettings.logger.debug(f"Uploading {filename} to {AppSettings.door43_bucket_name} bucket {key} …")
+                AppSettings.door43_s3_handler().upload_file(filepath, key, cache_time=0)
 
         # Now we place json files and redirect index.html for the whole repo to this index.html file
-        GlobalSettings.logger.info("Copying json files and setting up redirect…")
+        AppSettings.logger.info("Copying json files and setting up redirect…")
         try:
-            GlobalSettings.door43_s3_handler().copy(from_key=f'{s3_repo_key}/project.json', from_bucket=GlobalSettings.cdn_bucket_name)
-            GlobalSettings.door43_s3_handler().copy(from_key=f'{s3_commit_key}/manifest.json',
+            AppSettings.door43_s3_handler().copy(from_key=f'{s3_repo_key}/project.json', from_bucket=AppSettings.cdn_bucket_name)
+            AppSettings.door43_s3_handler().copy(from_key=f'{s3_commit_key}/manifest.json',
                                                     to_key=f'{s3_repo_key}/manifest.json')
-            GlobalSettings.door43_s3_handler().redirect(key=s3_repo_key, location='/' + s3_commit_key)
-            GlobalSettings.door43_s3_handler().redirect(key=s3_repo_key + '/index.html',
+            AppSettings.door43_s3_handler().redirect(key=s3_repo_key, location='/' + s3_commit_key)
+            AppSettings.door43_s3_handler().redirect(key=s3_repo_key + '/index.html',
                                                         location='/' + s3_commit_key)
             self.write_data_to_file_and_upload_to_CDN(output_dir, s3_commit_key, fname='deployed', data=' ')  # flag that deploy has finished
         except Exception as e:
-            GlobalSettings.logger.critical(f"Deployer threw an exception: {e}: {traceback.format_exc()}")
+            AppSettings.logger.critical(f"Deployer threw an exception: {e}: {traceback.format_exc()}")
 
         elapsed_seconds = int(time.time() - start)
-        GlobalSettings.logger.debug(f"Deploy completed in {elapsed_seconds} seconds.")
+        AppSettings.logger.debug(f"Deploy completed in {elapsed_seconds} seconds.")
         self.close()
         return True
     # end of ProjectDeployer.deploy_revision_to_door43(build_log)
@@ -132,7 +132,7 @@ class ProjectDeployer:
 
     def template_converted_files(self, build_log:str, output_dir:str, repo_name:str, resource_type:str, s3_commit_key:str,
                                  source_dir:str, start_time, template_filepath:str):
-        GlobalSettings.logger.debug(f"template_converted_files(…, od={output_dir}, '{repo_name}'," \
+        AppSettings.logger.debug(f"template_converted_files(…, od={output_dir}, '{repo_name}'," \
                                    f" '{resource_type}', k={s3_commit_key}, sd={source_dir}," \
                                    f" {start_time}, tf={template_filepath}) with {self.unzip_dir}…")
         assert 'errors' in build_log
@@ -141,7 +141,7 @@ class ProjectDeployer:
         source_dir = self.unzip_dir
         html_files = sorted(glob(os.path.join(source_dir, '*.html')))
         if len(html_files) < 1:
-            GlobalSettings.logger.warning("No html files found by ProjectDeployer.template_converted_files!")
+            AppSettings.logger.warning("No html files found by ProjectDeployer.template_converted_files!")
             content = ""
             if build_log['errors']:
                 content += """
@@ -174,8 +174,8 @@ class ProjectDeployer:
             self.run_templater(templater)
             success = True
         except Exception as e:
-            GlobalSettings.logger.error(f"Error applying template {template_filepath} to resource type {resource_type}:")
-            GlobalSettings.logger.error(f'{e}: {traceback.format_exc()}')
+            AppSettings.logger.error(f"Error applying template {template_filepath} to resource type {resource_type}:")
+            AppSettings.logger.error(f'{e}: {traceback.format_exc()}')
             self.close()
             success = False
 
@@ -183,11 +183,11 @@ class ProjectDeployer:
             # Update index of templated files
             index_json_fname = 'index.json'
             index_json = self.get_templater_index(s3_commit_key, index_json_fname)
-            # GlobalSettings.logger.debug(f"Initial 'index.json': {json.dumps(index_json)[:256]}")
+            # AppSettings.logger.debug(f"Initial 'index.json': {json.dumps(index_json)[:256]}")
             self.update_index_key(index_json, templater, 'titles')
             self.update_index_key(index_json, templater, 'chapters')
             self.update_index_key(index_json, templater, 'book_codes')
-            # GlobalSettings.logger.debug(f"Final 'index.json': {json.dumps(index_json)[:256]} …")
+            # AppSettings.logger.debug(f"Final 'index.json': {json.dumps(index_json)[:256]} …")
             self.write_data_to_file_and_upload_to_CDN(output_dir, s3_commit_key, index_json_fname, index_json)
         return source_dir, success
     # end of ProjectDeployer.template_converted_files function
@@ -197,8 +197,8 @@ class ProjectDeployer:
         out_file = os.path.join(output_dir, fname)
         write_file(out_file, data)
         key = s3_commit_key + '/' + fname
-        GlobalSettings.logger.debug(f"Uploading '{fname}' to {GlobalSettings.cdn_bucket_name} {key} …")
-        GlobalSettings.cdn_s3_handler().upload_file(out_file, key, cache_time=0)
+        AppSettings.logger.debug(f"Uploading '{fname}' to {AppSettings.cdn_bucket_name} {key} …")
+        AppSettings.cdn_s3_handler().upload_file(out_file, key, cache_time=0)
     # end of ProjectDeployer.write_data_to_file_and_upload_to_CDN function
 
 
@@ -214,17 +214,17 @@ class ProjectDeployer:
 
         Adds entries to the index_json_dict
         """
-        # GlobalSettings.logger.debug(f"ProjectDeployer.update_index_key({index_json_dict}, , '{key_string}')")
+        # AppSettings.logger.debug(f"ProjectDeployer.update_index_key({index_json_dict}, , '{key_string}')")
         data = index_json_dict[key_string]
         data.update(getattr(templater_object, key_string))
         index_json_dict[key_string] = data
-        # GlobalSettings.logger.debug(f"ProjectDeployer.update_index_key now has {index_json_dict}")
+        # AppSettings.logger.debug(f"ProjectDeployer.update_index_key now has {index_json_dict}")
     # end of ProjectDeployer.update_index_key function
 
 
     @staticmethod
     def get_templater_index(s3_commit_key:str, index_json_fname:str) -> dict:
-        index_json = GlobalSettings.cdn_s3_handler().get_json(s3_commit_key + '/' + index_json_fname)
+        index_json = AppSettings.cdn_s3_handler().get_json(s3_commit_key + '/' + index_json_fname)
         if not index_json:
             index_json['titles'] = {}
             index_json['chapters'] = {}
