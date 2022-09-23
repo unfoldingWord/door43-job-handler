7ffe635d798b509b18da54c7dd82bd9ae48e3fcb
Richard Mahn <richmahn@users.noreply.github.com>
2022-09-23 02:32:49 -0600

    Updates payload to have commit_hash

--- webhook.py
+++ webhook.py
@@ -788,6 +788,7 @@ def handle_page_build(base_temp_dir_name:str, submitted_json_payload:Dict[str,An
             'repo_name': repo_name,
             'repo_owner': repo_owner_username,
             'repo_ref': commit_id,
+            'commit_hash': commit_hash,
             'repo_data_url': repo_data_url,
             'dcs_domain': dcs_domain,
             'resource_type': resource_subject, # This used to be rc.resource.identifier
